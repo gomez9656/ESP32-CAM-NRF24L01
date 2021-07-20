3415e47f4a7cd83747a99e4d1c913aed9e4682cb
Juan Pablo  Gómez García <juanpg9656@gmail.com>
2021-07-19 20:14:41 -0500

    receiving 5-10 pixel data ok, but not working with 100, probably a bug

--- esp8266-receiver/esp8266-receiver.ino
+++ esp8266-receiver/esp8266-receiver.ino
@@ -23,7 +23,6 @@ RH_NRF24 nrf24(2, 4);
 uint8_t start_com[] = "start communication";
 
 //to store the buffer_length sended by the transmitter
-char char_buffer_length[5];
 int buffer_length = 0;
 
 //to know which step of the communication are we in
@@ -39,7 +38,7 @@ void setup()
 }
 
 void loop()
-{
+{ 
   if (nrf24.available())
   {
     // Should be a message for us now   
@@ -47,36 +46,59 @@ void loop()
     uint8_t len = sizeof(buf);
     if (nrf24.recv(buf, &len))
     {
-      //if buf == start communication
+      //following the communication steps, this are the pixels one by one
+      if(counter > 1){
+
+        image[0 + (counter - 2)] = atoi((char*)buf);
+        counter++;
+      }
+
+      //if counter == to this, it means we reached the final pixel
+      if(counter == 7){
+
+        for(int i = 0; i < 100; i++){
+          Serial.println(image[i]);
+        }
+        
+        //free the allocated memory for the image buffer
+        free(image);
+        counter = 0;
+        Serial.println("finish transmission");
+      }
+      
+      //following the communication steps, counter == 1 is the buffer length
+      if(counter == 1){
+      
+        Serial.print("got request: ");
+        Serial.println((char*)buf);
+        buffer_length = atoi((char*)buf);
+        //allocate a very big memory for the image buffer
+        image = (uint8_t*)malloc(buffer_length);
+
+        counter++;
+      }
+    
+      //if buf == "start communication"
       //then the communication is starting
       if(memcmp(buf, start_com, 20) == 0){
         
         Serial.println("Communication starting");
         counter++;
-      }
-      //following the steps, counter == 1 would be buffer length
-      if(counter == 1){
 
-        memcpy(char_buffer_length, buf, 5);
-        Serial.println(char_buffer_length);
-        buffer_length = atoi(char_buffer_length);
-
-        //allocate a very big memory for the image buffer
-        image = (uint8_t*)malloc(buffer_length);
+        Serial.print("got request: ");
+        Serial.println((char*)buf);
       }
-      
+
+      /*
       Serial.print("got request: ");
       Serial.println((char*)buf);
- 
+      */
     }
     else
     {
       Serial.println("recv failed");
     }
   }
-
-  //free the allocated memory for the image buffer
-  free(image);
 }
 
 /*
